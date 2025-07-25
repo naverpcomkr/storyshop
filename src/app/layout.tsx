3c4b2fa98a8ca74de485247edcb5dd501196494c
naverpcomkr <naverpcomkr@gmail.com>
2025-07-25 09:48:24 +0900

    favicon 및 기타 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,11 +24,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        {/* ...기존 head 내용... */}
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
     </html>
+  );
+}
+
+export function Head() {
+  return (
+    <>
+      <link rel="icon" href="/favicon.ico" />
+    </>
   );
 }
